8a92bee958806d2398e8d7e95b077815538a902d
Roopamdeep Kaur <roopam_kaur@yahoo.in>
2021-02-04 22:53:19 -0500

    added expense tracker components

---
+++ src/components/Balance.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+const Balance = () => {
+  return (
+    <React.Fragment>
+      <h4>Your Balance</h4>
+      <h1>$0.00</h1>
+    </React.Fragment>
+  );
+};
+
+export default Balance;
